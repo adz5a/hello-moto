04c7a0377be044ec1462754b847be233d2b1d7e9
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-07-18 21:06:57 +0200

    add new components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { Form } from "components/Form";
 
 class App extends Component {
     render() {
         return (
-            <div>hello</div>
+            <div>
+                <Form />
+            </div>
         );
     }
 }
---
+++ src/components/Form.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+
+
+export function Form () {
+
+    return (
+        <form action="sign-up_submit" method="get" acceptCharset="utf-8">
+            <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                <legend className="ph0 mh0 fw6 clip">Sign Up</legend>
+                <div className="mt3">
+                    <InputWithLabel
+                        name="baseURL"
+                        type="text"
+                        label="Base URL"
+                    />
+                </div>
+                <div className="mt3">
+                    <InputWithLabel
+                        name="bucketname"
+                        type="text"
+                        label="Bucket Name"
+                    />
+                </div>
+            </fieldset>
+            <Button type="submit" />
+        </form>
+    );
+
+}
+
+export function Input ( { type, name } ) {
+
+    return (
+        <input
+            className="b pa2 input-reset ba bg-transparent"
+            type={type}
+            name={name}
+        />
+    );
+
+}
+
+export function InputWithLabel ( {
+    type,
+    name,
+    label
+} ) {
+
+    const id = [ type, name, label ].join("-");
+    return (
+        <span>
+            <label className="db fw4 lh-copy f6" htmlFor={id}>{label}</label>
+            <input
+                className="b pa2 input-reset ba bg-transparent"
+                type={type}
+                name={name}
+                id={id}
+            />
+
+    </span>
+    );
+
+}
+
+export function Button ( { type, value } ) {
+
+    return(
+        <input
+            type={type || "button"}
+            className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6"
+            value={value}
+        />
+    );
+
+}
