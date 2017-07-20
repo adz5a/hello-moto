0086ced38ab72d0613bf1ff193ac4946114a6905
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-07-20 18:53:31 +0200

    add ragger

--- src/components/Drop.js
+++ src/components/Drop.js
@@ -1,17 +1,55 @@
 import React, { Component } from "react";
+import forEach from "lodash/forEach";
 
 
 const dropStyle = {
     border: "solid",
-    width: "50px",
-    height: "50px"
+    width: "500px",
+    height: "500px"
 };
+
+
+function onDrop ( e ) {
+
+    e.preventDefault();
+    const { dataTransfer } = e;
+    console.log(dataTransfer.types);
+    
+    console.log(dataTransfer);
+    
+    forEach(dataTransfer.files, file => {
+
+
+        console.log(file.name);
+        console.log(file.size);
+        console.log(file.webkitRelativePath);
+
+
+
+    });
+
+}
+
+function onDragOver ( e ) {
+
+    e.preventDefault();
+    
+
+
+}
 export class Drop extends Component {
 
     render () {
 
         return (
-            <div style={dropStyle}>hello</div>
+            <div>
+            <div 
+                style={dropStyle}
+                onDrop={onDrop}
+                onDragOver={onDragOver}
+            >hello</div>
+            <input onChange={onDrop} type="file"/>
+            </div>
         );
 
     }
