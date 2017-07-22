28ef0dcc48dd39948a6ee2b4e58445ad71b223b8
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-07-22 17:52:43 +0200

    update styles

--- src/components/styles.js
+++ src/components/styles.js
@@ -4,6 +4,6 @@ export const defaultBordered = "b ph3 pa2 ba b--black bg-transparent f6";
 
 export const linkStyle = joinClasses(defaultBordered, "dim", "link", "black");
 
-export const inputStyle = joinClasses(defaultBordered, "input-reset", "pointer", "grow");
+export const inputStyle = joinClasses(defaultBordered, "input-reset", "pointer");
 
 export const centerFlex ="flex flex-column items-center";
