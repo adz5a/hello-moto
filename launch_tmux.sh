c9a966ff5e424d4a228492ca5b9d7dbd65ed980b
antoine zimmermann <antoine.zimmermann@mobilespot.com>
2017-07-22 21:38:43 +0200

    fix config tmux

--- launch_tmux.sh
+++ launch_tmux.sh
@@ -21,7 +21,7 @@ tmux send-keys "yarn start" C-m
 # create windows and launch vim
 tmux new-window -t $SESSION:1 -n "editor" 
 tmux select-pane -t 0
-tmux send-keys "vim src/" C-m
+tmux send-keys "cd src && vim ." C-m #to avoid PATH issues
 
 # select second window
 tmux select-window -t $SESSION:1
