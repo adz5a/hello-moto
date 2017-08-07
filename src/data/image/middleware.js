5c1640cd14def14a7ffc1ad1e61e08833bd3535a
adz5a <antoine.d.zimmermann@gmail.com>
2017-08-07 18:11:24 +0200

    add proto middleware for images

---
+++ src/data/image/middleware.js
@@ -0,0 +1,27 @@
+import { createStreamMiddleware } from "data/streamMiddleware";
+import {
+    FIND_DOC
+} from "data/db";
+import xs from "xstream";
+import {
+    Map 
+} from "immutable";
+
+
+export const creator = ( action$, state$ ) => {
+
+    return xs.of({
+        type: FIND_DOC,
+        data: {
+            query: Map({
+                selector: {
+                    type: "image"
+                }
+            })
+        }
+    })
+
+};
+
+
+export const middleware = createStreamMiddleware(creator, "image");
