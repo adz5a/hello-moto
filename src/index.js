1791526c3ba0cf4626a81d5886b036a08acbfaf0
antoine zimmermann <antoine.zimmermann@storetail.com>
2017-08-27 12:50:01 +0200

    set tachyon as explixit app deps

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // import './index.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+import "tachyons/css/tachyons.css";
 import { renderApp } from "components/renderApp";
 
 renderApp(App, document.getElementById("root"));
