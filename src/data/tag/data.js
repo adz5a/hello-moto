3046eeb89d04a4e409dee93586752dc68c2784fc
antoine zimmermann <antoine.zimmermann@storetail.com>
2017-09-01 01:01:32 +0200

    add constructor for data

--- src/data/tag/data.js
+++ src/data/tag/data.js
@@ -1,7 +1,18 @@
 import { ACTIONFACTORY } from "data/commons";
+import {
+    // Seq,
+    // List,
+    // Repeat,
+    Record
+} from "immutable";
+
+
+
 
 
 const prefixer = ACTIONFACTORY("__tag__");
 
+
+export const Tag = Record({});
 
 export const makeId = tagName => prefixer(tagName);
